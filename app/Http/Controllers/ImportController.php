7e1c4fc25e99ba18f40f86ff20396a395942f740
Goodarz Goodarznia <goodarznia@gmail.com>
2021-05-13 18:20:32 +0200

    * Add store method to ImportController.php

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -10,4 +10,15 @@ class ImportController extends Controller
     {
         return view('import');
     }
+    public function store(Request $request)
+    {
+        $validData = $request->validate(['data_file' => 'required|file']);
+        if ($_FILES['data_file']['type'] == 'application/json') {
+            //if file type == json then do below
+        } else {
+            // if file type is not json do below
+            return back()->withErrors(['badFile' => 'Please upload JSON file']);
+        }
+
+    }
 }
